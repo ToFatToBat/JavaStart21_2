ae913cb59086e8d87e96b391b2dcdd5da19d98f2
ToFatToBat <kpapiernia@wp.pl>
2020-08-16 17:44:29 +0200

    Zmiany

--- src/main/java/calculator/CalculatorServlet.java
+++ src/main/java/calculator/CalculatorServlet.java
@@ -4,18 +4,39 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @WebServlet("/calculator")
 class CalculatorServlet extends HttpServlet {
+    int pharseMeter;
+    int pharseCentymeter;
+    int pharseMilimeter;
 
-    protected void doGet (HttpServletRequest request, HttpServletResponse response) {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String meter = request.getParameter("meter");
-        String cemtymeter = request.getParameter("centymeter");
+        String centymeter = request.getParameter("centymeter");
         String milimeter = request.getParameter("milimeter");
+
+        pharseMeter = Integer.valueOf(meter);
+        pharseCentymeter = Integer.valueOf(centymeter);
+        pharseMilimeter = Integer.valueOf(milimeter);
+
+
+            java.io.PrintWriter writer = response.getWriter();
+            writer.println(pharseMeter);
+
+
+        System.out.println(pharseCentymeter);
+        System.out.println(pharseMeter);
+        System.out.println(pharseMilimeter);
 
 
     }
 
+//    public static choosenParameter(int pharseMeter, int pharseCentymeter, int pharseMilimeter) {
+
 
 }
+
